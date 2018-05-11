91ed4e287c176d72240a71fea1f45c673142c590
Alexey Alekhin <laughedelic@gmail.com>
2018-05-12 01:02:53 +0200

    Fix comments formatting

--- src/main/scala/probot/context.scala
+++ src/main/scala/probot/context.scala
@@ -14,38 +14,35 @@ trait Context extends js.Object {
   /** A logger */
   val log: LoggerWithTarget = js.native
 
-  /** Returns a boolean if the actor on the event was a bot */
+  /** @return `true` if the actor on the event was a bot */
   def isBot(): Boolean = js.native
 
-  /**
-   * Return the `owner` and `repo` params for making API requests against a
-   * repository.
-   *
-   * @param extraFields Params to be merged with the repo params.
-   */
+  /** @return an object with the `owner` and `repo` params for making API
+    * requests against a repository.
+    *
+    * @param extraFields Params to be merged with the repo params.
+    */
   def repo(
     extraFields: js.Object = js.native
   ): ContextRepo = js.native
 
-  /**
-   * Return the `owner`, `repo`, and `number` params for making API requests
-   * against an issue or pull request. The object passed in will be merged with
-   * the repo params.
-   *
-   * @param extraFields Params to be merged with the issue params.
-   */
+  /** Return the `owner`, `repo`, and `number` params for making API requests
+    * against an issue or pull request. The object passed in will be merged with
+    * the repo params.
+    *
+    * @param extraFields Params to be merged with the issue params.
+    */
   def issue(
     extraFields: js.Object = js.native
   ): ContextIssue = js.native
 
-  /**
-   * Reads the app configuration from the given YAML file in the `.github`
-   * directory of the repository.
-   *
-   * @param fileName Name of the YAML file in the `.github` directory
-   * @param defaultConfig An object of default config options
-   * @return Configuration object read from the file
-   */
+  /** Reads the app configuration from the given YAML file in the `.github`
+    * directory of the repository.
+    *
+    * @param fileName Name of the YAML file in the `.github` directory
+    * @param defaultConfig An object of default config options
+    * @return Configuration object read from the file
+    */
   def config(
     fileName: String,
     defaultConfig: js.Object = js.native
--- src/main/scala/probot/robot.scala
+++ src/main/scala/probot/robot.scala
@@ -9,33 +9,31 @@ trait Robot extends js.Object {
   /** A logger */
   val log: LoggerWithTarget = js.native
 
-  /**
-   * Listen for [GitHub webhooks](https://developer.github.com/webhooks/),
-   * which are fired for almost every significant action that users take on
-   * GitHub.
-   *
-   * @param event the name of the [GitHub webhook
-   * event](https://developer.github.com/webhooks/#events). Most events also
-   * include an "action". For example, the [`issues`](
-   * https://developer.github.com/v3/activity/events/types/#issuesevent)
-   * event has actions of `assigned`, `unassigned`, `labeled`, `unlabeled`,
-   * `opened`, `edited`, `milestoned`, `demilestoned`, `closed`, and `reopened`.
-   * Often, your bot will only care about one type of action, so you can append
-   * it to the event name with a `.`, like `issues.closed`.
-   * @param callback a function to call when the webhook is received.
-   */
+  /** Listen for [[https://developer.github.com/webhooks GitHub webhooks]],
+    * which are fired for almost every significant action that users take on
+    * GitHub.
+    *
+    * @param event the name of the
+    * [[https://developer.github.com/webhooks/#events GitHub webhook event]].
+    * Most events also include an "action". For example, the
+    * [[https://developer.github.com/v3/activity/events/types/#issuesevent
+    * `issues`]] event has actions of `assigned`, `unassigned`, `labeled`,
+    * `unlabeled`, `opened`, `edited`, `milestoned`, `demilestoned`, `closed`,
+    * and `reopened`. Often, your bot will only care about one type of action,
+    * so you can append it to the event name with a `.`, like `issues.closed`.
+    * @param callback a function to call when the webhook is received.
+    */
   def on(
     event: String | js.Array[String],
     callback: Context => Unit
   ): Unit = js.native
 
-  /**
-   * Get an [[link http://expressjs.com express]] router that can be used to
-   * expose HTTP endpoints
-   *
-   * @param path the prefix for the routes
-   * @return an express [[express.Router]] to expose new HTTP endpoints
-   * @see [[http://expressjs.com/en/4x/api.html#router]]
-   */
+  /** Get an [[http://expressjs.com express]] router that can be used to expose
+    * HTTP endpoints.
+    *
+    * @param path the prefix for the routes
+    * @return an express [[express.Router]] to expose new HTTP endpoints
+    * @see [[http://expressjs.com/en/4x/api.html#router]]
+    */
   def route(path: String): express.Router = js.native
 }
